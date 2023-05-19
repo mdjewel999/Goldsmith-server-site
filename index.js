28427635f34e999c182786735510b94b2b9a7480
mdjewel999 <mdjewel878761@gmail.com>
2023-05-19 22:47:01 +0600

    toy service code added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // console.log(process.env.DB_PASS);
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ydthzao.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -26,12 +27,61 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyServiceCollection = client.db('toyHouse').collection('toyService'); 
+    const toyBookingCollection = client.db('toyHouse').collection('toyService');
+
+    //
+app.get('/toyService', async(req, res) =>{
+  const cursor = toyServiceCollection.find();
+  const result = await cursor.toArray();
+  res.send(result)
+})
+
+
+//
+app.get('/toyService/:id', async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) }
+
+
+app.get('/toyService', async(req, res) =>{
+  console.log(req.query);
+  const result =await toyBookingCollection.find().toArray();
+  res.send(result)
+})
+
+
+  //
+  app.post('/toyBookings', async (req, res) => {
+    const toyBooking = req.body;
+    console.log(toyBooking);
+    const result = await toyBookingCollection.insertOne(toyBooking);
+    console.log(result); 
+    res.send(result);
+  });
+
+
+
+
+  const options = {
+      // Include only the `title` and `imdb` fields in the returned document
+      projection: { title: 1, price: 1, service_id: 1, img: 1, description: 1, 
+        category_title: 1 },
+  };
+
+  const result = await toyServiceCollection.findOne(query, options);
+  res.send(result);
+})
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
