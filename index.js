6fe632beb173f9186e76225f8a96537187b2336e
mdjewel999 <mdjewel878761@gmail.com>
2023-05-21 13:56:29 +0600

    toy collection added code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,7 +11,6 @@ app.use(express.json());
 
 // console.log(process.env.DB_PASS);
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ydthzao.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,35 +27,83 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const serviceCollection = client.db('housetoyes').collection('carhouse'); 
+    const serviceCollection = client.db("housetoyes").collection("carhouse");
+    const toyCollection = client.db("housetoyes").collection("toybooking");
 
-    
-app.get('/carhouse', async(req, res) =>{
-  const cursor = serviceCollection.find();
-  const result = await cursor.toArray();
-  res.send(result)
-})
+    //
+    app.get("/carhouse", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    //
+    app.get("/carhouse/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
 
-app.get('/carhouse/:id', async(req, res) =>{
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { category_title: 1, price: 1, ratings: 1, img: 1 },
+      };
+
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
+
+    // toy bookings
+    app.get("/toybooking", async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await toyCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/toybooking", async (req, res) => {
+      const booking = req.body;
+      console.log(booking);
+      const result = await toyCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    //Delate
+
+app.patch("/toybooking/:id", async(req, res) =>{
   const id = req.params.id;
-  const query ={_id: new ObjectId(id)}
-
-  const options = {
-    // Include only the `title` and `imdb` fields in the returned document
-    projection: { category_title: 1, price: 1, ratings: 1, img: 1  },
+  const filter = {_id: new ObjectId(id)}
+  const updatedBookingToy = req.body;
+  console.log(updatedBookingToy);
+  const updateDoc = {
+    $set: {
+        status: updatedBookingToy.status
+    },
 };
-
-  const result = await serviceCollection.findOne(query, options);
-  res.send(result)
+const result = await toyCollection.updateOne(filter, updateDoc);
+  res.send(result);
 })
 
 
-  
+
+    app.delete("/toybooking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.deleteOne(query);
+      res.send(result)
+    });
+
+
+
+
+
 
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -64,12 +111,10 @@ app.get('/carhouse/:id', async(req, res) =>{
 }
 run().catch(console.dir);
 
-
-
 app.get("/", (req, res) => {
-    res.send("happy toy house is running");
-  });
-  
-  app.listen(port, () => {
-    console.log(`happy toy house Server is running on port ${port}`);
-  });
+  res.send("happy toy house is running");
+});
+
+app.listen(port, () => {
+  console.log(`happy toy house Server is running on port ${port}`);
+});
