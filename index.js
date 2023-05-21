c087c2da68db9577c2eb403a1d38e976ecd98ed8
mdjewel999 <mdjewel878761@gmail.com>
2023-05-21 20:48:16 +0600

    toybooking id change code update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// console.log(process.env.DB_PASS);
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ydthzao.mongodb.net/?retryWrites=true&w=majority`;
 
